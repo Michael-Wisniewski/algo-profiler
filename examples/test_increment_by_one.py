357bba2c2eae3840b06168c784f14c95428ac1ee
Michał Wiśniewski <mikex929@gmail.com>
2021-06-28 21:33:14 +0200

    Update examples.

--- examples/test_increment_by_one.py
+++ examples/test_increment_by_one.py
@@ -51,81 +51,81 @@ def naive_increment_by_one(numbers_list):
 
 profiler = Profiler()
 
-# profiler.run_tests(func=increment_by_one, test_set=test_set)
+profiler.run_tests(func=increment_by_one, test_set=test_set)
 
-# profiler.run_stress_tests(
-#     func=increment_by_one,
-#     naive_func=naive_increment_by_one,
-#     data_gen=data_gen,
-#     gen_min_arg=1,
-#     gen_max_arg=100,
-#     gen_steps=10,
-# )
+profiler.run_stress_tests(
+    func=increment_by_one,
+    naive_func=naive_increment_by_one,
+    data_gen=data_gen,
+    gen_min_arg=1,
+    gen_max_arg=100,
+    gen_steps=10,
+)
 
-# profiler.run_coverage(func=increment_by_one, test_set=test_set)
+profiler.run_coverage(func=increment_by_one, test_set=test_set)
 
-# profiler.run_time_check(
-#     func=increment_by_one,
-#     kwargs=data_gen(10000000),
-#     iterations=10
-# )
+profiler.run_time_check(
+    func=increment_by_one,
+    kwargs=data_gen(10000000),
+    iterations=10
+)
 
-# profiler.run_cProfile(
-#     func=increment_by_one,
-#     kwargs=data_gen(10000000)
-# )
+profiler.run_cProfile(
+    func=increment_by_one,
+    kwargs=data_gen(100000)
+)
 
 # profiler.run_snakeviz(
 #     func=increment_by_one,
 #     kwargs=data_gen(10000000)
 # )
 
-# profiler.run_line_profiler(
-#     func=increment_by_one,
-#     kwargs=data_gen(10000000)
-# )
+profiler.run_line_profiler(
+    func=increment_by_one,
+    kwargs=data_gen(10000)
+)
 
-# profiler.run_time_analysis(
-#     func=increment_by_one,
-#     data_gen=data_gen,
-#     gen_min_arg=10,
-#     gen_max_arg=1000000,
-#     gen_steps=10,
-#     find_big_o=True
-# )
+profiler.run_time_analysis(
+    func=increment_by_one,
+    data_gen=data_gen,
+    gen_min_arg=10,
+    gen_max_arg=1000000,
+    gen_steps=10,
+    find_big_o=True
+)
 
-# profiler.run_memory_check(
-#     func=increment_by_one,
-#     kwargs=data_gen(10000000)
-# )
+profiler.run_memory_check(
+    func=increment_by_one,
+    kwargs=data_gen(10000)
+)
 
 profiler.run_memory_profiler(
     func=increment_by_one,
-    kwargs=data_gen(10000),
+    kwargs=data_gen(100),
     clean_result=True
 )
 
-# profiler.run_time_based_memory_usage(
-#     func=increment_by_one,
-#     kwargs=data_gen(10000000)
-# )
+profiler.run_time_based_memory_usage(
+    func=increment_by_one,
+    kwargs=data_gen(1000000)
+)
 
-# profiler.run_check_memory_leaks(
-#     func=increment_by_one,
-#     kwargs=data_gen(100000)
-# )
+profiler.run_check_memory_leaks(
+    func=increment_by_one,
+    kwargs=data_gen(1000)
+)
 
-# profiler.run_memory_analysis(
-#     func=increment_by_one,
-#     data_gen=data_gen,
-#     gen_min_arg=100,
-#     gen_max_arg=1000,
-#     gen_steps=10,
-#     find_big_o=True
-# )
+profiler.run_memory_analysis(
+    func=increment_by_one,
+    data_gen=data_gen,
+    gen_min_arg=100,
+    gen_max_arg=1000,
+    gen_steps=10,
+    find_big_o=True
+)
 
-# profiler.run_scalene(
-#     func=increment_by_one,
-#     kwargs=data_gen(1000000),
-#     cpu_sampling_rate=0.001
-# )
+profiler.run_scalene(
+    func=increment_by_one,
+    kwargs=data_gen(100),
+    cpu_sampling_rate=0.001
+)
--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
 
 long_description = """\
+![lint](https://raw.githubusercontent.com/gist/Michael-Wisniewski/3f54787a495dfd8a05072c1eb12f9da3/raw/lint.svg)
+![lines of code](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/gist/Michael-Wisniewski/3f54787a495dfd8a05072c1eb12f9da3/raw/lines_of_code.json)
+![coverage](https://raw.githubusercontent.com/gist/Michael-Wisniewski/3f54787a495dfd8a05072c1eb12f9da3/raw/coverage.svg)
+![python version](https://raw.githubusercontent.com/gist/Michael-Wisniewski/3f54787a495dfd8a05072c1eb12f9da3/raw/python_version.svg)
+
 ## algo-profiler
 
 Package of profiling tools which allows to run function:
@@ -21,7 +26,7 @@ Package of profiling tools which allows to run function:
 
 setup(
     name="algo-profiler",
-    version="0.0.21",
+    version="0.0.23",
     description="A module for profiling algorithms.",
     author="Michał Wiśniewski",
     long_description=long_description,
@@ -36,6 +41,7 @@ setup(
         "Documentation": "https://aroundpython.com/2021/06/13/tool-for-writing-algorithms/",
     },
     packages=["algo_profiler"],
+    include_package_data=True,
     install_requires=[
         "big_o",
         "coverage",
